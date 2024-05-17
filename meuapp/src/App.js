59976983af3594f1a11153dc48ada1c88244e181
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-05-17 19:13:08 -0300

    att

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,39 +1,30 @@
 import React, { Component } from 'react';
+import Feed from './components/Membro/feed';
 
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state ={
-      status: false
+      feed: [
+        {id: 1, username: 'Wesley', curtidas: 10, comentarios: 2},
+        {id: 2, username: 'Lucas', curtidas: 120, comentarios: 24},
+        {id: 3, username: 'Isabela', curtidas: 30, comentarios: 12},
+        {id: 4, username: 'João', curtidas: 1, comentarios: 0},
+        
+      ]
     }
-    this.sair = this.sair.bind(this);
-    this.entrar = this.entrar.bind(this);
-
-
   }
-
-  sair(){
-    this.setState({status: false});
-  }
-
-  entrar(){
-    this.setState({status: true})
-  };
-
     render(){
       return(
         <div>
-          {this.state.status ?
-          <div>
-            <h2>Bem vindo ao sistema</h2>
-            <button onClick={this.sair}>Sair</button>
-          </div> :
-          <div>
-            <h2>Olá visitante, faça o login!</h2>
-            <button onClick={this.entrar}>Entrar no sistema</button>
-          </div>      
-        }
+
+          {this.state.feed.map((item)=>{
+            
+            return(
+              <Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
+            );
+          })}
         </div>
       )
     }
